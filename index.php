e8ab661d80b1953d6f915124712ddce202b04e65
David Langenberg <davel@uchicago.edu>
2013-07-24 16:41:43 -0600

    put result info in proper spot

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
         "http://www.w3.org/TR/html4/loose.dtd">
 <?php 
 
+$errors = "";
+$results = "";
+
 function special_formatting($input) {
     $output = htmlspecialchars($input, ENT_QUOTES);
     $output = str_replace(array('  ', "\n"), array('&nbsp;&nbsp;', '<br>'), $output);
@@ -67,12 +70,6 @@ if(isset($_POST['submit'])){
    $filename = nameFile($_FILES['userfile']['tmp_name']);             // create the name of the file based on the entityID
    $ext = substr($filename, strrpos($filename,'.'));      // Get the extension from the filename.
  
-   $errors = "";
-   
-// We don't want to bother checking file types
-//   if(!in_array($ext,$allowed_filetypes))
-//      die('<font color=\'red\'>The file type you attempted to upload is not allowed.  You must upload a .xml file.</font>');
- 
    if(filesize($_FILES['userfile']['tmp_name']) > $max_filesize)
       $errors .= '<font color=\'red\'>The file you attempted to upload is too large.  Please ensure the file is less than 40k and try again.</font><br/>';
 
@@ -86,15 +83,15 @@ if(isset($_POST['submit'])){
   if (strlen($errors) == 0) {
 
         if (move_uploaded_file($_FILES['userfile']['tmp_name'], $upload_path . $filename)) {
-            echo 'Your metadata was uploaded successfully.  Please proceed to <a href="configure.html">configuration</a> and <a href="test.html">testing</a>. <br /> <br />';                        // It worked
-            echo 'Your complete metadata is below.  You don\'t need to understand the entire file, but it\'s helpful to recognize your entityID in the first element below, as well as your provider\'s certificate.  <a href="https://wiki.shibboleth.net/confluence/display/SHIB2/Home" target="_new">The Shibboleth wiki</a> can help you <a href="https://wiki.shibboleth.net/confluence/display/SHIB2/Metadata" target="_new">learn about metadata</a>.<br /> <br /> <br /> <p><tt>';
+            $results .= 'Your metadata was uploaded successfully.  Please proceed to <a href="configure.html">configuration</a> and <a href="test.html">testing</a>. <br /> <br />';                        // It worked
+            $results .= 'Your complete metadata is below.  You don\'t need to understand the entire file, but it\'s helpful to recognize your entityID in the first element below, as well as your provider\'s certificate.  <a href="https://wiki.shibboleth.net/confluence/display/SHIB2/Home" target="_new">The Shibboleth wiki</a> can help you <a href="https://wiki.shibboleth.net/confluence/display/SHIB2/Metadata" target="_new">learn about metadata</a>.<br /> <br /> <br /> <p><tt>';
 
             // Build display array of metadata
             exec('xmlstarlet fo -o ' . $upload_path . $filename, $displayMetadata);
             foreach ($displayMetadata as $value) {
-                echo (special_formatting($value) . '<br />');
+                $results .= (special_formatting($value) . '<br />');
             }
-            echo '</tt></p> <br />';
+            $results .= '</tt></p> <br />';
             shell_exec('sleep 3; sh /opt/cpm.sh'); // Build metadata file after giving a moment to ensure upload completed
         } else {
             die('<font color=\'red\'>There was an error during the file upload.  Please try again.</font>');     // It failed
@@ -133,6 +130,10 @@ if(isset($_POST['submit'])){
     <?php
     echo $errors;
     ?>
+</div>
+
+<div id="results">
+    <?php echo $results; ?>
 </div>
 <br />
 <br />
